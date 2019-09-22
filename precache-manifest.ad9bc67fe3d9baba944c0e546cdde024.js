3df58a26fd35a66c3e4cc4da99c4635daaafefac
Allen Liu <alex03108861@gmail.com>
2019-09-22 21:23:07 +0800

    Updates

---
+++ precache-manifest.ad9bc67fe3d9baba944c0e546cdde024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1e4221ea4dfbbda98a2be61923f7a7",
+    "url": "/React_Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "7433025ebd6968728d1b",
+    "url": "/React_Tic-Tac-Toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "6f9390ae9359531c0ba1",
+    "url": "/React_Tic-Tac-Toe/static/js/2.9caf0fd4.chunk.js"
+  },
+  {
+    "revision": "7433025ebd6968728d1b",
+    "url": "/React_Tic-Tac-Toe/static/js/main.ae9f9b64.chunk.js"
+  },
+  {
+    "revision": "d4b0437d36ccecfaa5dc",
+    "url": "/React_Tic-Tac-Toe/static/js/runtime-main.308c5c64.js"
+  }
+]);
